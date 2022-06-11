ee4e6df31fed60d8381f690e6a218a96ada92c1d
gazes12 <khozhainovdmitro@gmail.com>
2022-06-11 18:44:37 +0300

    add light and dark themes

--- js/burger.js
+++ js/burger.js
@@ -2,5 +2,6 @@ const menu = document.querySelector('.header__menu');
 document.addEventListener('click', (e) =>{
     if(e.target.classList.contains('burger')){
         menu.classList.toggle('active');
+        e.target.classList.toggle('active');
     }
 })
---
+++ js/switcher.js
@@ -0,0 +1,29 @@
+const switchers = document.querySelectorAll('.change-theme');
+switchers.forEach(switcher => {
+    switcher.addEventListener('click', () =>{
+        applyTheme(switcher.dataset.theme);
+    })
+});
+
+function applyTheme(themeName){
+    let themeUrl = `css/themes/${themeName}.css`;
+    document.querySelector('[title="theme"]').setAttribute('href', themeUrl);
+    localStorage.setItem('theme', themeName);
+}
+
+let activeTheme = localStorage.getItem('theme');
+if(activeTheme === null){
+    applyTheme('dark');
+}else{
+    applyTheme(activeTheme);
+}
+
+const windowWidth = window.innerWidth;
+const menuList = document.querySelector('.header__menu__list');
+if(windowWidth < 815){
+    const theme = document.querySelector('.theme');
+    menuList.append(theme);
+}else{
+    const headerContainer = document.querySelector('.header__container');
+    headerContainer.append(theme);
+}
